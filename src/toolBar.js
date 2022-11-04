30a1e4a0d04c786787d4411969dc752ff8539abd
kamal98ETH <bestkacm@gmail.com>
2022-11-04 18:33:41 +0100

    feat: notification and time bar

--- src/toolBar.js
+++ src/toolBar.js
@@ -7,6 +7,10 @@ function ToolBar() {
                 <img src={windowsLogo} alt="windows xp logo" />
                 <p>Start</p>
             </button>
+            <div id="tabs"></div>
+            <div id="time-notif">
+                <p id="time">18:00</p>
+            </div>
         </div>
     )
 }
