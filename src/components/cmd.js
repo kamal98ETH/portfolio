40e07e2122736241af12c82a40dd4272d66dc797
kamal98ETH <bestkacm@gmail.com>
2022-11-23 00:45:38 +0100

    feat: dynamic typing speed

--- src/components/cmd.js
+++ src/components/cmd.js
@@ -4,32 +4,24 @@ import { useEffect, useState } from "react";
 function Cmd(props) {
     // console.log(props)
     let data_content = props.data.content;
-    // let data_content = [
-    //     {
-    //         type: "paragraph",
-    //         data: "test1 some random text here"
-    //     },
-    //     // {
-    //     //     type: "break"
-    //     // },
-    //     {
-    //         type: "link",
-    //         data: "link to google",
-    //         href: "https://www.google.com"
-    //     },
-    //     {
-    //         type: "paragraph",
-    //         data: "test1 some random text here"
-    //     }
-    // ];
+
+    //set a dynamic typing speed depending on number of letters to be displayed
+    const ANIMATION_TIME = 5000;
+    let number_of_letters = 0;
+    for (let index in data_content) {
+        if (data_content[index].data) {
+            number_of_letters += data_content[index].data.length
+        }
+        console.log(data_content[index].data)
+    }
+    let interval = ANIMATION_TIME / number_of_letters;
+
     //new variable that will copy data content but letter by letter to form the typing animation
     let [paragraph_content, setParagraphContent] = useState([])
     //index representing the letter we are currently copying
     let [data_content_str_index, setDataContentStrIndex] = useState(0)
     //index representing the data_content item we are currently copying
     let [data_content_array_index, setDataContentArrayIndex] = useState(0)
-
-
 
     useEffect(() => {
         // check if we did not exceed the length of data coming from props
@@ -50,7 +42,7 @@ function Cmd(props) {
                             setDataContentArrayIndex(data_content_array_index + 1)
                             setDataContentStrIndex(0)
                         }
-                    }, 50)
+                    }, interval)
                 }
                 //check if we are receiving a break element
             } else {
@@ -60,34 +52,23 @@ function Cmd(props) {
         }
     }, [data_content_str_index])
 
+    let key = -1
     let text_to_display = paragraph_content.map((line) => {
-        let key = paragraph_content.indexOf(line)
+        key++
         if (data_content[key].type == "paragraph") {
             return <p key={key}>{line}</p>
         } else if (data_content[key].type == "link") {
             return <a key={key} href={data_content[key].href} target="_blank">{line}</a>
         } else if (data_content[key].type == "break") {
-            console.log('br')
-            return <br />
+            return <br key={key} />
         }
     })
-
-    // useEffect(() => {
-    //     if (index < data_content.length) {
-    //         setTimeout(() => {
-    //             setContent(content + data_content[index])
-    //             setIndex(index + 1)
-    //         }, 50)
-    //     }
-    // }, [index, content])
 
     return (
         <div className="cmd" style={{ zIndex: props.z_index }}>
             <WindowHeader data={props.data} minimize_id={props.minimize_id} maximize_id={props.maximize_id} close_id={props.close_id} action={props.action} />
             <div className="cmd-body">
                 {text_to_display}
-                {/* {paragraph_content} */}
-                {/* {data_content_str_index} */}
             </div>
         </div>
     )
